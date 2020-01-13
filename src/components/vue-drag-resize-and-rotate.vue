a3e71218a6d05315c0886270e74d0f50bb9f9d46
kalitk0 <gatevv@gmail.com>
2020-01-13 17:23:25 +0200

    Update vue-drag-resize-and-rotate.vue

--- src/components/vue-drag-resize-and-rotate.vue
+++ src/components/vue-drag-resize-and-rotate.vue
@@ -1,3 +1,3 @@
-<template src="./vue-drag-resize-and-rotate.html"></template>
+<template src="./vue-drag-resize-and-rotate.htm"></template>
 <script src="./vue-drag-resize-and-rotate.js"></script>
 <style src="./vue-drag-resize-and-rotate.css"></style>
